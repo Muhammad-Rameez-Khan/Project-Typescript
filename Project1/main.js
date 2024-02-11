9715c6e40438e8468dee175a4f4e9f14124eb347
Muhammad Rameez Khan <digitechmrk@gmail.com>
2024-02-11 08:00:47 +0500

    Additional configration  and js file added config.json added

---
+++ Project1/main.js
@@ -0,0 +1,9 @@
+// Set up 
+//  Install VSCode  Node.js Check status 
+//  then create a folder then open VScode then create a file with any name but make sure the extention will be .ts 
+//  for example i have create MediaDeviceInfo.ts  
+// Then compile it with js file 
+
+
+
+console.log("Hello World");
